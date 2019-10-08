9c70bfa0e953970d7dc66c5c84d7888d1476157d
Bruno Vieira F <brunno.vieeira@hotmail.com>
2019-10-08 20:16:57 -0300

    bolo

---
+++ any.py
@@ -0,0 +1,8 @@
+INGREDIENTES BOLO DE CENOURA:
+3 cenouras médias raspadas e picadas
+3 ovos
+1 xícara de óleo
+2 xícaras de açúcar
+2 xícaras de farinha de trigo
+1 colher (sopa) de fermento em pó
+1 pitada de sal
